bf64c8e43c49b441855b909a30e1ad0148cc8dc5
Paradox Programming <nathaniel.fargo@gmail.com>
2019-05-03 07:19:17 -0600

    BUNCH OF STUFF

--- sketch.js
+++ sketch.js
@@ -1,37 +1,31 @@
 var transPoint = new Player(0);
 
-var speedMult = 15;
-var defSpeedMult = speedMult;
-
-var canvas;
+var canvas, speedSlider;
 
 function setup() {
 	canvas = createCanvas(windowWidth, windowHeight)
-	background(200, 225, 255);
+	speedSlider = createSlider(1, 20, 1, 1);
+	speedSlider.position(20,20, 500)
+	speedSlider.addClass("slider")
+	speedSlider.style("width", "40%");
 	var AIapart = abs(AI.getX(0)-AI.getX(1));
 	var AIlead = abs(AI.getX(0)-AIapart);
 	AI.init(floor((width/2-AIlead)/AIapart));
 };
 
 function draw() {
-	
-	if(mouseIsPressed){
-		speedMult = 2;
-	} else {
-		speedMult = defSpeedMult;
-	}
-	
+
 	updateCanvas();
 
 	background(200, 225, 255);
 
 	translate(width / 2, height / 2);
-	
-	for(var i = 0; i < speedMult; i ++){
+
+	for(var i = 0; i < speedSlider.value(); i ++){
 		if(i!=0){
 			frameCount++;
 		}
-		
+
 		transPoint.update();
 		player.run();
 		AI.run();
@@ -42,16 +36,16 @@ function draw() {
 		}
 
 	}
-	
+
 	push();
 	translate(-transPoint.x, 0);
-	
+
 	player.display();
 	AI.display();
 	Obstacle.display();
-	
+
 	pop();
-	
+
 	push();
 	fill(100, 60, 0);
 	stroke(0,200,0);
@@ -71,9 +65,9 @@ function reset() {
 };
 
 function updateCanvas() {
-    if(canvas.height != windowHeight || canvas.width != windowWidth){
-	resizeCanvas(windowWidth, windowHeight)
-    }
+	if(canvas.height != windowHeight || canvas.width != windowWidth){
+		resizeCanvas(windowWidth, windowHeight)
+	}
 };
 
 function windowResized() {
